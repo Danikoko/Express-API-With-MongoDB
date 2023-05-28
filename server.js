ad2ab87cf6ca85aa2c62d5faa3fc6ffba0740f4d
Daniel <denamudu@bariansystems.com>
2023-05-28 18:08:31 +0100

    I have written code for creating products using the Product model. Part C of CRUD is working.

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const MONGODB_PASSWORD = '6Kri5M7jgggFv86T';
 const MONGODB_URI = `mongodb+srv://root:${MONGODB_PASSWORD}@danikokoapi.d5xg6bc.mongodb.net/?retryWrites=true&w=majority`;
+
+const Product = require('./models/productModel');
+
+/** Indicate Express app can accept JSON */
+app.use(express.json());
+/** Indicate Express app can accept JSON */
 
 /** Routes */
 app.get('/', (req, res) => {
@@ -27,17 +33,29 @@ app.get('/blog', (req, res) => {
         }
     );
 });
-/** Routes */
 
-app.listen(3000, () => {
-    console.log('Node API app is running on port 3000.');
+app.post('/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+
+    } catch (ERROR) {
+        console.log(ERROR.message);
+        res.status(500).json({
+            message: ERROR.message
+        });
+    }
 });
+/** Routes */
 
 /** MongoDB connection */
 mongoose
 .connect(MONGODB_URI)
 .then(() => {
     console.log('Connected to MongoDB successfully.');
+    app.listen(3000, () => {
+        console.log('Node API app is running on port 3000.');
+    });
 })
 .catch((ERROR) => {
     console.log(ERROR);
