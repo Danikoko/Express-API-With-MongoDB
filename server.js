59700a85a8ae5114455d797fd41f1c05a94726b6
Daniel <denamudu@bariansystems.com>
2023-05-28 18:09:21 +0100

    I have written code for fetching and returning all products using the Product model. Part R of CRUD is working.

--- server.js
+++ server.js
@@ -34,6 +34,17 @@ app.get('/blog', (req, res) => {
     );
 });
 
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (ERROR) {
+        res.status(500).json({
+            message: ERROR.message
+        });
+    }
+});
+
 app.post('/products', async (req, res) => {
     try {
         const product = await Product.create(req.body);
