2b09abb473e49bbeafff63d873f8a310777359ea
Daniel <denamudu@bariansystems.com>
2023-05-29 22:36:48 +0100

    Added a way for developers to conveniently and securely add MongoDB passwords.

--- config/constants.ts
+++ config/constants.ts
@@ -1,8 +1,14 @@
-const MONGODB_PASSWORD: string = 'ENTER_YOUR_PASSWORD_HERE';
+/** Import your MONGODB_PASSWORD */
+import __SENSITIVE_DATA__ from "./sensitiveData";
+const { MONGODB_PASSWORD } = __SENSITIVE_DATA__;
+/** Import your MONGODB_PASSWORD */
+
+// Or add here
+// const MONGODB_PASSWORD: string = 'ENTER_PASSWORD_HERE';
 const MONGODB_URI: string = `mongodb+srv://root:${MONGODB_PASSWORD}@danikokoapi.d5xg6bc.mongodb.net/?retryWrites=true&w=majority`;
 
 const __CONSTANTS__ = {
-    // MONGODB_PASSWORD,
+    MONGODB_PASSWORD,
     MONGODB_URI
 }
 
