bc8765c497ed66889c5cedd75db6e10769939bb3
Daniel <denamudu@bariansystems.com>
2023-05-29 21:57:16 +0100

    I have setup a standard Express API with Typescript.

--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,4 @@
-const MONGODB_PASSWORD: string = '6Kri5M7jgggFv86T';
+const MONGODB_PASSWORD: string = 'ENTER_YOUR_PASSWORD_HERE';
 const MONGODB_URI: string = `mongodb+srv://root:${MONGODB_PASSWORD}@danikokoapi.d5xg6bc.mongodb.net/?retryWrites=true&w=majority`;
 
 const __CONSTANTS__ = {
